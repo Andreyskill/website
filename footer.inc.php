20e4d09b9aff4299d0e0f8b5c0688cc8ca574140
Andreyskill <ooobrauer@gmail.com>
2022-11-04 16:17:55 +0300

    Initial commit

---
+++ footer.inc.php
@@ -0,0 +1,3 @@
+<div class="footer">
+            <h2>Мы изучаем PHP!</h2>
+        </div>
---
+++ logo.inc.php
@@ -0,0 +1,5 @@
+    
+                <div class="logo"> 
+                <img src="img/l.jfif" alt="php">
+                </div>                                                 
+        
